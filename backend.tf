f61a132b1e947b305d739a9532d0f2b8885eff1d
mihaela202 <66444567+mihaela202@users.noreply.github.com>
2020-08-31 20:16:43 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "dev-mvmamazon.com"
+        bucket = "jenkins-terraform-evolvecybertraining"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
